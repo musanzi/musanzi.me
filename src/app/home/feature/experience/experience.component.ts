791bf5249e388784aa522e1c5b96d1f7392195ef
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-04 11:53:26 +0200

    refactor: improve layout and structure of experience component

--- src/app/home/feature/experience/experience.component.ts
+++ src/app/home/feature/experience/experience.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { experiences } from './utils/data/experiences.data';
+import { LucideAngularModule, MoveUpRight } from 'lucide-angular';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './experience.component.html'
 })
 export class ExperienceComponent {
   experiences = experiences;
+  icons = {
+    moveUpRight: MoveUpRight
+  };
 }
