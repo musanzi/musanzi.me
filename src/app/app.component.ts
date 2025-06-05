aa31f8f357c2075decf65a286881b3abc9466844
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-05 16:27:03 +0200

    chore: update deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './shared/ui/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, LoaderComponent],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, TitleStrategy, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { PageTitleStrategy } from './page-title.strategy';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      })
-    ),
-    provideAnimations(),
-    provideClientHydration(withEventReplay()),
     { provide: TitleStrategy, useClass: PageTitleStrategy },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
   ],
 };
