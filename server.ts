174287a0460234992b58c55d8d823e60d6245ef1
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-05 13:47:30 +0200

    refactor: update default server port from 3000 to 7000

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 3000;
+  const port = process.env['PORT'] || 7000;
 
   // Start up the Node server
   const server = app();
