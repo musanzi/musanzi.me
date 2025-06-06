99bee332f65033002e9cacfbe1131f7f420d9c5d
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-06 09:49:20 +0200

    fix: update wording in experiences for clarity and consistency

--- src/app/landing/utils/data/experiences.ts
+++ src/app/landing/utils/data/experiences.ts
@@ -6,25 +6,25 @@ export const experiences: IExperience[] = [
     company: 'ZX Connect',
     location: 'Lubumbashi, DR Congo',
     description:
-      'Developed and maintained web applications using React and Next.js, focusing on performance and user experience. Collaborated with designers to implement responsive designs and ensure cross-browser compatibility.',
+      'Built responsive and performant user interfaces using Next.js and React. Collaborated with designers to implement modern UI/UX designs and ensured cross-device compatibility.',
     websiteUrl: 'https://www.linkedin.com/company/zx-connect-sarl/',
     duration: {
       from: 'November 2020',
-      to: 'September 2020'
+      to: 'September 2021',
     },
-    tools: ['typescript', 'react', 'nextjs', 'firebase', 'tailwindcss', 'git', 'github']
+    tools: ['typescript', 'react', 'nextjs', 'tailwindcss', 'git', 'github'],
   },
   {
-    position: 'Lead Fullstack Developer',
+    position: 'Lead Software Developer',
     company: "Centre d'innovation de Lubumbashi",
     location: 'Lubumbashi, DR Congo',
     description:
-      'Led the development of a web application for managing local businesses, utilizing Angular and NestJS. Implemented RESTful APIs and integrated with a MariaDB database. Focused on performance optimization and security best practices.',
+      'Led fullstack development of scalable web applications using Angular and NestJS. Designed secure authentication systems, dynamic forms, user workflows for entrepreneurial programs and others apps for the community.',
     websiteUrl: 'https://cinolu.org',
     duration: {
       from: 'November 2023',
-      to: 'Present'
+      to: 'Present',
     },
-    tools: ['typescript', 'angular', 'nestjs', 'mariadb', 'tailwindcss', 'git', 'github', 'vps']
-  }
+    tools: ['typescript', 'angular', 'nestjs', 'mariadb', 'tailwindcss', 'git', 'github'],
+  },
 ];
