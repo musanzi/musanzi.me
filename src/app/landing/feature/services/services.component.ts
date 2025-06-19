01cce8c7627f3d3c585793b8aaa3929ea2012b5f
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-19 11:16:18 +0200

    feat: add services data and update services component for dynamic rendering

--- src/app/landing/feature/services/services.component.ts
+++ src/app/landing/feature/services/services.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { services } from '../../utils/data/services';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-services',
-  imports: [],
-  templateUrl: './services.component.html'
+  imports: [LucideAngularModule],
+  templateUrl: './services.component.html',
 })
-export class ServicesComponent {}
+export class ServicesComponent {
+  services = services;
+}
