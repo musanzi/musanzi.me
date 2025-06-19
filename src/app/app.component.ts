d170822ca29d77c2ebbd2d0e6fa7d287c532febc
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-19 10:33:31 +0200

    refactor: remove loader component and simplify app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoaderComponent } from './shared/ui/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
