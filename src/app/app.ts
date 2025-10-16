c4b970b2662ca6434c27f382a1fdbeff39cb42fc
Wilfried Musanzi <musanziw.m@gmail.com>
2025-10-16 10:04:07 +0200

    fix: remove unused Footer import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html'
 })
 export class App {}
